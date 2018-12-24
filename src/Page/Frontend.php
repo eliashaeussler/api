87920a887e7ecd144cf5c4012f9c26254f7defa9
Elias Häußler <mail@elias-haeussler.de>
2018-12-24 23:17:01 +0100

    [FEATURE] Improve Frontend class to support multiple message types

--- src/Page/Frontend.php
+++ src/Page/Frontend.php
@@ -13,21 +13,96 @@ namespace EliasHaeussler\Api\Page;
  */
 class Frontend
 {
+    /** @var string Message type for successful messages */
+    const MESSAGE_TYPE_SUCCESS = "success";
+
+    /** @var string Message type for notices */
+    const MESSAGE_TYPE_NOTICE = "notice";
+
+    /** @var string Message type for warnings */
+    const MESSAGE_TYPE_WARNING = "warning";
+
+    /** @var string Message type for error messages */
+    const MESSAGE_TYPE_ERROR = "error";
+
+
+    /**
+     * @todo add doc
+     *
+     * @param string $content
+     * @return string
+     */
     public static function bootstrap(string $content): string
     {
         return sprintf('<div class="page">%s</div>', $content);
     }
 
     /**
+     * @todo add doc
+     *
+     * @param string $header
+     * @param string $body
+     * @param string $type
+     * @return string
+     */
+    public static function message(string $header, string $body, string $type = self::MESSAGE_TYPE_NOTICE): string
+    {
+        $messageHeader = sprintf('<div class="message__header">%s</div>', $header);
+        $messageBody = sprintf('<div class="message__body">%s</div>', $body);
+        $message = sprintf('<div class="message message--%s">%s%s</div>', $type, $messageHeader, $messageBody);
+
+        return self::bootstrap($message);
+    }
+
+    /**
+     * @todo add doc
+     *
+     * @param string $header
+     * @param string $body
+     * @return string
+     */
+    public static function success(string $header, string $body): string
+    {
+        return self::message($header, $body, self::MESSAGE_TYPE_SUCCESS);
+    }
+
+    /**
+     * @todo add doc
+     *
+     * @param string $header
+     * @param string $body
+     * @return string
+     */
+    public static function notice(string $header, string $body): string
+    {
+        return self::message($header, $body);
+    }
+
+    /**
+     * @todo add doc
+     *
+     * @param string $header
+     * @param string $body
+     * @return string
+     */
+    public static function warning(string $header, string $body): string
+    {
+        $header = sprintf("Warning: %s", $header);
+
+        return self::message($header, $body, self::MESSAGE_TYPE_WARNING);
+    }
+
+    /**
+     * @todo add doc
+     *
      * @param \Exception $object
      * @return string
      */
     public static function error(\Exception $object): string
     {
-        $errorHeader = sprintf('<div class="error__header">Error: %s</div>', get_class($object));
-        $errorBody = sprintf('<div class="error__body">%s [%s]</div>', $object->getMessage(), $object->getCode());
-        $content = sprintf('<div class="error">%s%s</div>', $errorHeader, $errorBody);
+        $header = sprintf("Error: %s", get_class($object));
+        $body = sprintf("%s [%s]", $object->getMessage(), $object->getCode());
 
-        return self::bootstrap($content);
+        return self::message($header, $body, self::MESSAGE_TYPE_ERROR);
     }
 }
