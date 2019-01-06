fed0fafa2cd92150b707ae52edfff4a80fa06147
Elias Häußler <mail@elias-haeussler.de>
2019-01-06 16:31:55 +0100

    [FEATURE] Add trait to require user environment for API controllers
    Related: #17

---
+++ src/Controller/UserEnvironmentRequired.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Copyright (c) 2019 Elias Häußler <mail@elias-haeussler.de>. All rights reserved.
+ */
+declare(strict_types=1);
+namespace EliasHaeussler\Api\Controller;
+
+use EliasHaeussler\Api\Utility\GeneralUtility;
+
+/**
+ * Set requirement of user environment for specific API controllers.
+ *
+ * This trait can be used to load the user environment for a specific API controller. When using it, the environment
+ * will be initialized right before the request is handled.
+ *
+ * @package EliasHaeussler\Api\Controller
+ * @author Elias Häußler <mail@elias-haeussler.de>
+ * @license MIT
+ */
+trait UserEnvironmentRequired
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function initializeEnvironment()
+    {
+        $envFile = sprintf("%s.env", $this->getControllerName());
+        GeneralUtility::loadEnvironment($envFile);
+    }
+}
