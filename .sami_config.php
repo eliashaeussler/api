ceee3ef149c930fe3577b95e9db1385af378d864
Elias Häußler <mail@elias-haeussler.de>
2019-05-05 23:48:45 +0200

    [TASK] Update paths in Sami configuration file

--- .sami_config.php
+++ .sami_config.php
@@ -2,16 +2,16 @@
 /**
  * Copyright (c) 2019 Elias Häußler <elias@haeussler.dev>. All rights reserved.
  */
-define('CLASSES_PATH', 'src');
-define('SOURCE_PATH', __DIR__ . '/' . CLASSES_PATH);
-define('DOCS_DIR', __DIR__ . '/docs');
+define('ROOT_PATH', __DIR__);
+define('SOURCE_PATH', ROOT_PATH . '/src');
+define('DOCS_PATH', ROOT_PATH . '/docs');
 
 $iterator = \Symfony\Component\Finder\Finder::create()
     ->files()
     ->name('*.php')
     ->in(SOURCE_PATH);
 
-$versions = (new \Sami\Version\GitVersionCollection(__DIR__))
+$versions = (new \Sami\Version\GitVersionCollection(ROOT_PATH))
     ->addFromTags(function ($version) {
         return strpos($version, '0') != 0;
     })
@@ -20,8 +20,8 @@ $versions = (new \Sami\Version\GitVersionCollection(__DIR__))
 return new \Sami\Sami($iterator, [
     'versions' => $versions,
     'title' => 'elias-haeussler.de API',
-    'build_dir' => DOCS_DIR . '/php/%version%',
-    'cache_dir' => DOCS_DIR . '/cache/%version%',
-    'remote_repository' => new \Sami\RemoteRepository\GitHubRemoteRepository('eliashaeussler/api', __DIR__),
+    'build_dir' => DOCS_PATH . '/php/%version%',
+    'cache_dir' => DOCS_PATH . '/cache/%version%',
+    'remote_repository' => new \Sami\RemoteRepository\GitHubRemoteRepository('eliashaeussler/api', ROOT_PATH),
     'default_opened_level' => 2,
 ]);
